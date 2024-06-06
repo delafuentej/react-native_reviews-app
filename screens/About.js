02fd7b998ca1dceecdd9473ea7729c46e904c4a2
delafuentej <jfuentelucas@gmail.com>
2024-06-06 16:39:16 +0200

    setting up

---
+++ screens/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function About(){
+    return(
+        <View>
+            <Text>About</Text>
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+
+});
+
---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home(){
+    return(
+        <View>
+            <Text style={styles.container}>Home</Text>
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+    container:{
+        padding:20,
+    }
+
+});
+
